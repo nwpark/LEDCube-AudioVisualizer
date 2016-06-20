9345ec76f85c3e67be7253a2ef053af3f5e6a603
Nick <nick.park@student.manchester.ac.uk>
2016-06-20 23:00:11 +0100

    added comments

--- AudioVisualizerArduino/AudioVisualizerArduino.ino
+++ AudioVisualizerArduino/AudioVisualizerArduino.ino
@@ -2,18 +2,23 @@
 #include <CubeInterface.h>
 
 CubeInterface cube(1);
+// holds the array retrieved from serial input
 byte inBuffer[64];
 byte pattern[8][8];
 
 void setup()
 {
+  // high baud rate to minimize delay and subsequently
+  // flickering
   Serial.begin(250000);
+  
   for(int i=0; i < 64; i++)
     inBuffer[i] = 0;
 }
 
 void loop()
 {
+  // delay 10 milliseconds
   cube.wait(10);
   
   // request an update
@@ -23,28 +28,44 @@ void loop()
 // process an update - method called when there is serial input
 void serialEvent()
 {
+  // read the serial input (expecting 64 bytes)
+  // store the bytes in an array
   Serial.readBytes(inBuffer, sizeof(inBuffer));
-  
+
+  // update the cube display based on the input
   if(inBuffer != NULL)
   {
+    // apparantly arduino executes additional for loop
+    // statements before the first itteration so i=-1
     int i=-1;
     for(int x=0; x < 8; x++)
     {
+      // helps with flickering
       cube.writeCube();
+      
       for(int y=0; y < 8; y++, i++)
       {
-        // helps with flickering
-        cube.clearColumn(x, y);
-        
-        if(inBuffer[i] > 8)
-          inBuffer[i] = 8;
-        int height = inBuffer[i];
-        while(height > 0)
-        {
-          cube.light(x, y, height-1);
-          height--;
-        }
-      }
-    }
-  }
-}
+        setColumnHeight(x, y, inBuffer[i]);
+      } // for
+    } // for
+  } // if
+} // serialEvent
+
+void setColumnHeight(byte x, byte y, byte height)
+{
+  // turn off all leds in the column before writing
+  // the new height to it
+  cube.clearColumn(x, y);
+  
+  // max height is 8
+  if(height > 8)
+    height = 8;
+  
+  // turn on all leds in thecolumn from 0 up to height 
+  while(height > 0)
+  {
+    cube.light(x, y, height-1);
+    height--;
+  } // while
+} // setColumnHeight
+
