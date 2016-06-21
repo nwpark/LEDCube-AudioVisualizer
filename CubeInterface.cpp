7856f52692117f71d81d5fb86b0e15f304cd7062
Nick <nick.park@student.manchester.ac.uk>
2016-06-21 01:57:30 +0100

    added comments

--- CubeInterface.cpp
+++ CubeInterface.cpp
@@ -1,5 +1,6 @@
 #include "CubeInterface.h"
 
+// array to map anode pin columns of leds to output pins on shift registers
 const byte ANODE_PINS[64][2] =
   {{6, 0}, {5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}, {7, 0},
    {6, 2}, {5, 2}, {4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}, {7, 2},
@@ -10,12 +11,15 @@ const byte ANODE_PINS[64][2] =
    {6, 3}, {5, 3}, {4, 3}, {3, 3}, {2, 3}, {1, 3}, {0, 3}, {7, 3},
    {6, 1}, {5, 1}, {4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}, {7, 1}};
 
+// array to map cathode pin columns of leds to output pins on shift registers
 const byte CATHODE_PINS[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 
+// array to hold value for each led in the cube (on or off)
 boolean ledStatus[8][8][8];
 byte currentLayer = 0;
 byte cubeDelay;
 
+// shift register control pins
 const byte CLEAR_PIN = 12;
 const byte CLOCK_PIN = 11;
 const byte LATCH_PIN = 10;
@@ -44,16 +48,19 @@ CubeInterface::CubeInterface(byte requiredDelay)
 // destructor
 CubeInterface::~CubeInterface(){}
 
+// turn a specific led on
 void CubeInterface::light(byte x, byte y, byte z)
 {
   ledStatus[x][y][z] = HIGH;
 } // light
 
+// turn a specific led off
 void CubeInterface::off(byte x, byte y, byte z)
 {
   ledStatus[x][y][z] = LOW;
 } // off
 
+// turn all leds off
 void CubeInterface::clearAll()
 {
   for(byte x = 0; x < 8; x++)
@@ -62,12 +69,14 @@ void CubeInterface::clearAll()
         ledStatus[x][y][z] = LOW;
 } // clearAll
 
+// turn all leds in a specific column off
 void CubeInterface::clearColumn(byte x, byte y)
 {
   for(byte z=0; z < 8; z++)
     ledStatus[x][y][z] = LOW;
 } // clearColumn
 
+// shift a high bit (1) into the shift registers
 void CubeInterface::highBit()
 {
   digitalWrite(DATA_PIN, HIGH);
@@ -76,20 +85,27 @@ void CubeInterface::highBit()
   digitalWrite(DATA_PIN, LOW);
 } // highBit
 
+// shift a low bit (0) into the shift registers
 void CubeInterface::lowBit()
 {
   digitalWrite(CLOCK_PIN, HIGH);
   digitalWrite(CLOCK_PIN, LOW);
 } // lowBit
 
+// latch data stored in the shift registers to the outputs
 void CubeInterface::latch()
 {
   digitalWrite(LATCH_PIN, HIGH);
   digitalWrite(LATCH_PIN, LOW);
 } // latch
 
+// write the values stored in ledStatus to the cube, ideally this method should
+// be called atleast once every millisecond for multiplexing to give a smooth
+// image and avoid flickering.
+// displays 1 layer at a time at a high frequency to give a smooth image.
 void CubeInterface::writeCube()
 {
+  // 9th shift register controls cathodes
   for(byte i = 0; i < 8; i++)
   {
     if(CATHODE_PINS[i] == currentLayer)
@@ -98,6 +114,7 @@ void CubeInterface::writeCube()
       lowBit();
   } // for
 
+  // first 8 shift registers control anodes
   for(byte i = 0; i < 64; i++)
     if(ledStatus[ANODE_PINS[i][0]]
                 [ANODE_PINS[i][1]]
@@ -114,6 +131,8 @@ void CubeInterface::writeCube()
   latch();
 } // writeCube
 
+// allow for delays in the code by updating the cube every 10 microseconds
+// rather than completely pausing the program.
 void CubeInterface::wait(byte t)
 {
   while(t > 0)
